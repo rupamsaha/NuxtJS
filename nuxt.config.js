c3eecf834bba1ab57984abe0acd29dfc2010f70d
rupamsaha <rupam.saha@glovoapp.com>
2022-01-14 19:34:16 +0100

    new commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,7 @@ export default {
         },
         algolia: {  
             appID: "BWH88H6F6O",
-            key: "3db055b3104e4c5f4c2aaa13c9a6ab06",
+            key: "",
         },
         cloudinary: {
             apiKey: '577171994417629',
@@ -49,10 +49,10 @@ export default {
     privateRuntimeConfig : {
         algolia: {
             appID: "BWH88H6F6O",
-            key: "a2deb0a3f2ea619deaeb8f3729fdffc2",
+            key: "",
         },
         cloudinary: {
-            apiSecret: 'QhQnUtgtWTjm2u3rLlLhgxYZzPk'
+            apiSecret: ''
         }
     }
 }
